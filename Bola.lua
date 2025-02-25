3397ca33509e93ee3e7b99043df00e3a3a803a86
Gleidson Domiciano <58370373+domi900@users.noreply.github.com>
2025-02-25 10:59:17 -0300

    Revert "Merge branch 'main' into love-phisics"
    This reverts commit ed5b080ff78208389cbf92df2abc4305d7f2bc02, reversing
    changes made to 5ae633cd9a1711c4386596c8d091d4525958096f.

--- Bola.lua
+++ Bola.lua
@@ -1,14 +1,16 @@
 
 Bola = Class{}
 
-function Bola:init(x, y, radius, tag)
+function Bola:init(x, y, radius, tag, world)
     self.x = x
     self.y = y
-    self.raio = radius
+    self.r = radius
+    --self.width = width
+    --self.height = height
+    self.dx = 0
     self.tag = tag
-    self.diametro = self.raio * 2
-    self.world = world
 
+    self.world = world
 
     self.dy = 50
     self.dx = 0
@@ -24,16 +26,11 @@ function Bola:init(x, y, radius, tag)
     self.body:setFixedRotation(true)
 
     self.fixture:setCategory(1)
-
-    self.dy = 200
-    self.dx = 0
-
     
 end
 
 function Bola:update(dt)
     
-
 
     self.x, self.y = self.body:getPosition()
 end
@@ -49,49 +46,13 @@ function Bola:aplicarForca(direcao)
         self.body:applyLinearImpulse( self.dx, self.dy, self.body:getX(), self.body:getY())
     end
     --self.body:applyForce( 0, -300 )
-
-    self.y = self.y + self.dy * dt
-    self.x = self.x + self.dx * dt
-
-    if self.x - self.raio <= 0 then
-        self.x = self.raio  -- Evita que a bola entre na parede
-        self.dx = math.abs(self.dx)  -- Inverte a direção para a direita
-    elseif self.x + self.raio >= 500 then
-        self.x = 500 - self.raio  -- Mantém dentro da tela
-        self.dx = -math.abs(self.dx)  -- Inverte a direção para a esquerda
-    end
-
-    -- Colisão com o teto
-    if self.y - self.raio <= 0 then
-        self.y = self.raio  -- Evita que a bola vá para fora da tela
-        self.dy = math.abs(self.dy)  -- Rebate para baixo
-    end
-end
-
-function Bola:aplicarForca(direcao)
-    self.dy = -200
-    if direcao == "direita" then
-        self.dx = math.random(10, 200)
-    elseif direcao == "esquerda" then
-        self.dx = math.random(-200, -10)
-    else    
-        self.dx = self.dx
-    end
-
 end    
 
-function Bola:getPosicao()
-    return self.x + self.diametro, self.y + self.diametro
-end
-
 function Bola:reset(x, y)
-    self.x = x
-    self.y = y
-    self.dy = 200
+    self.body:setPosition( x, y )
     
 end
 
 function Bola:render()
-    love.graphics.circle("fill", self.x, self.y, self.raio)
-    love.graphics.circle("line", self.x, self.y, self.raio + 2)
+    love.graphics.circle("fill", self.x, self.y, self.r)
 end
